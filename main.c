ef6057840508bdfda549e8444633fff4528ba096
Abdalla Mohamed <55021977+abdallam1007@users.noreply.github.com>
2021-11-20 22:50:03 +0300

    new code

--- main.c
+++ main.c
@@ -7,7 +7,19 @@
 #define PE0 (*((volatile uint32_t *)0x40024004))
 #define PE1 (*((volatile uint32_t *)0x40024008))
 #define PE2 (*((volatile uint32_t *)0x40024010))
-
+#define PE3 (*((volatile uint32_t *)0x40024020))
+#define PE4 (*((volatile uint32_t *)0x40024040))
+#define PE5 (*((volatile uint32_t *)0x40024080))
+  
+#define PA2 (*((volatile uint32_t *)0x40004010))
+#define PA3 (*((volatile uint32_t *)0x40004020))
+#define PA4 (*((volatile uint32_t *)0x40004040))
+#define PA5 (*((volatile uint32_t *)0x40004080))
+#define PA6 (*((volatile uint32_t *)0x40004100))
+#define PA7 (*((volatile uint32_t *)0x40004200))
+  
+#define PB0 (*((volatile uint32_t *)0x40005004))
+#define PB1 (*((volatile uint32_t *)0x40005008))
 
 /*
  * n = G F E D C B A
@@ -64,11 +76,67 @@ void PLLInit()
     SYSCTL_RCC2_R &= ~0x00000800;
 }
 
-void set(int i)
+void set(int r, int i)
 {
-  PE1 = 0X00;
-  PE0 = i;
-  PE1 = 0X02;
+  switch (r)
+  {
+    case 0:
+    {
+      PE1 = 0X00;
+      PE0 = i;
+      PE1 = 0X02;
+      break;
+    }
+    
+    case 1:
+    {
+      PE4 = 0X00;
+      if (i) PE3 = 0x08;
+      else PE3 = 0x00;
+      PE4 = 0X10;
+      break;
+    }
+    
+    case 2:
+    {
+      PA2 = 0X00;
+      if (i) PE5 = 0x20; // 00100000
+      else PE5 = 0x00;
+      PA2 = 0X04;
+      break;
+    }
+    
+    case 3:
+    {
+      PA4 = 0X00;
+      if (i) PA3 = 0x08; // 00100000
+      else PA3 = 0x00;
+      PA4 = 0X10;
+      break;
+    }
+    
+    case 4:
+    {
+      PA7 = 0X00;
+      if (i) PA6 = 0x40; // 01000000
+      else PA6 = 0x00;
+      PA7 = 0X80;
+      break;
+    }
+
+    case 5:
+    {
+      PB1 = 0X00;
+      if (i) PB0 = 0x01; // 01000000
+      else PB0 = 0x00;
+      PB1 = 0X02;
+      break;
+    }
+    
+    default:
+      break;
+  }
+  
   return;
 }
 
@@ -77,13 +145,10 @@ void display_LCD_row(int r, int lcd, int digit)
   for (int j = 0; j < 16; j++)
   {
     if (j >= 0 && j < 7)
-      set(segment[digit][j]);
+      set(r, segment[digit][j]);
     
-    else set(LCDs[lcd][j - 7]);
+    else set(r, LCDs[lcd][j - 7]);
   }
-  
-  //PE2 = 0X00;
-  //PE2 = 0X04;
   
   return;
 }
@@ -93,19 +158,22 @@ void update_LCD()
 {
   for (int d = 0; d < 10; d++)
   {
-    for (int lcd = 0; lcd < 9; lcd++)
+    for (int c = 0; c < 500; c++)
     {
-      //for (int c = 0; c < 5000; c++)
-      //{
-      for (int r = 0; r < 9; r++)
+      for (int lcd = 0; lcd < 9; lcd++)
       {
-        display_LCD_row(r, lcd, d); 
+        //for (int c = 0; c < 800; c++)`
+        //{
+          for (int r = 0; r < 9; r++)
+          {
+            display_LCD_row(r, lcd, d); 
+          }
+          PE2 = 0X00;
+          PE2 = 0X04;
+          PA5 = 0x00;
+          PA5 = 0x20;
+        //}
       }
-      
-      PE2 = 0X00;
-      PE2 = 0X04;
-      
-      //}
     }
   }
 }
@@ -126,10 +194,30 @@ int main(void)
   GPIO_PORTE_CR_R = 0xFF;
   GPIO_PORTE_AMSEL_R = 0x00; // disable analog functionality
   GPIO_PORTE_PCTL_R = 0x00000000; // Select GPIO mode in PCTL
-  GPIO_PORTE_DIR_R = 0x07; // Ports E0,1,2 are output, rest are input
+  GPIO_PORTE_DIR_R = 0x3F; // Ports E0,1,2,3,4,5 are output, rest are input
   GPIO_PORTE_AFSEL_R = 0x00; // Disable alternate functionality
   GPIO_PORTE_DEN_R = 0xFF; //Enable digital ports
-  
+
+  // Configuring Port A
+  SYSCTL_RCGCGPIO_R |= 0x01; // enable clock for PORT A
+  GPIO_PORTA_LOCK_R = 0x4C4F434B; // this value unlocks the GPIOCR register.
+  GPIO_PORTA_CR_R = 0xFF;
+  GPIO_PORTA_AMSEL_R = 0x00; // disable analog functionality
+  GPIO_PORTA_PCTL_R = 0x00000000; // Select GPIO mode in PCTL
+  GPIO_PORTA_DIR_R = 0xFC; // Ports E0,1,2 are output, rest are input  11111100
+  GPIO_PORTA_AFSEL_R = 0x00; // Disable alternate functionality
+  GPIO_PORTA_DEN_R = 0xFF; //Enable digital ports
+
+  // Configuring Port B
+  SYSCTL_RCGCGPIO_R |= 0x02; // enable clock for PORT B
+  GPIO_PORTB_LOCK_R = 0x4C4F434B; // this value unlocks the GPIOCR register.
+  GPIO_PORTB_CR_R = 0xFF;
+  GPIO_PORTB_AMSEL_R = 0x00; // disable analog functionality
+  GPIO_PORTB_PCTL_R = 0x00000000; // Select GPIO mode in PCTL
+  GPIO_PORTB_DIR_R = 0xFF; // Ports E0,1,2 are output, rest are input  11111100
+  GPIO_PORTB_AFSEL_R = 0x00; // Disable alternate functionality
+  GPIO_PORTB_DEN_R = 0xFF; //Enable digital ports
+
   while (1)
   {
     update_LCD();
